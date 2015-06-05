7b450d0af1bcb04dbd002973304d06981639f8fe
Paul LEBELTEL <paul.lebeltel@gmail.com>
2015-06-05 11:05:33 +0200

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,7 @@
 ### application.bat - 2015 - LEBELTEL Paul ###
-echo "version 1.1"
+echo "version 2"
 echo "Copyright company - 2015"
-echo "Build : 10:00"
+build_hour = "11h00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
